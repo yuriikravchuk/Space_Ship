a01236069b17fdd9d88b5e003df6da605ca4ccde
durkoUrko <85782097+durkoUrko@users.noreply.github.com>
2021-10-21 16:20:38 +0300

    Add enemy.

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField]private float _speed;
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.position += new Vector3(0, -_speed);
+    }
+}
